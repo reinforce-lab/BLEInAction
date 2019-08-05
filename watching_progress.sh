15bbd3ba4035b7f6b1171c9d8cede7686e358bd9
Akihiro Uehara <u-akihiro@reinforce-lab.com>
2019-08-05 09:40:54 +0900

    ファイル構成を整理。

--- watching_progress.sh
+++ watching_progress.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-cd /Users/u_akihiro/Desktop/Books/BluetoothLowEnergy
+cd /Users/u_akihiro/Desktop/Books/BLEInAction
 date '+%F' | tr -d "\n"                             >> progress_data.csv
 echo ','   | tr -d "\n"                             >> progress_data.csv
-wc ch*adoc | tail -n 1 | tr -s ' ' | cut -d ' ' -f4 >> progress_data.csv
+wc *re | tail -n 1 | tr -s ' ' | cut -d ' ' -f4     >> progress_data.csv
 
 osascript progress_data.scpt 
